359aae89ac6448522b63aca36ed47a1d80403a26
pavan sai <sathipavansai@gmail.com>
2026-07-26 18:20:00 +0530

    initial commit

--- arrays/tofindleftrotate.java
+++ arrays/tofindleftrotate.java
@@ -1,23 +1,24 @@
 package arrays;
-
 import java.util.Arrays;
-
 public class tofindleftrotate {
-    public static void leftrotate(int [] arr , int k ){
-        int [] temp = {1,2,3};
-        for(int i = k;i<=arr.length-1;i++){
-         arr[i-k]= arr[i];
+   public static void reverse(int [] arr,int left,int right){
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
-        for(int j = arr.length-k;j<=arr.length-1;j++){
-            arr[j] = temp[j-(arr.length-k)];
-        }
-        System.out.println(Arrays.toString(arr));
     }
-   public static void main(String[] args) {
-    int [] arr = {1,2,3,4,5,6};
-
-    // left rotate by 3 value 
-    int k =2;
-    leftrotate(arr,k);
-   }
+    public static void main(String[] args) {  
+        int [] arr = {1,2,3,4,5,6,7,8};
+        int k =3;         
+        int n = arr.length;
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        reverse(arr,0,n-1);
+         System.out.println(Arrays.toString(arr));
+        
 }
+}
+
